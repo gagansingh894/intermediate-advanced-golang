7eacbc5e03174781e06fed2930596c6f3b82b11f
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2021-12-21 18:40:56 +0530

    Concurrency Code Refactor

--- concepts/concurrencyoverview.go
+++ concepts/concurrencyoverview.go
@@ -6,10 +6,7 @@ import (
 	"time"
 )
 
-// FAN IN
-// ch chan <-  interface{} means that we only write to channel. Write only channel
-// ch <- chan interface{} means that we can only read from channel. Read only channel
-func fanInProducer(ch chan <- int, name string)  {
+func producer(ch chan <- int, name string)  {
 	for {
 		// generate random number
 		n := rand.Intn(100)
@@ -19,14 +16,18 @@ func fanInProducer(ch chan <- int, name string)  {
 		fmt.Printf("%s -> %d\n", name, n)
 		ch <- n
 	}
-	
+
 }
 
-func fanInConsumer(ch <- chan int, name string) {
+func consumer(ch <- chan int, name string) {
 	for n := range ch {
 		fmt.Printf("%s <- %d\n", name, n)
 	}
 }
+
+// FAN IN
+// ch chan <-  interface{} means that we only write to channel. Write only channel
+// ch <- chan interface{} means that we can only read from channel. Read only channel
 
 func FanInExample() {
 	var n int
@@ -35,9 +36,9 @@ func FanInExample() {
 	chanB := make(chan  int)
 	chanC := make(chan  int)
 
-	go fanInProducer(chanA, "Channel A")
-	go fanInProducer(chanB, "Channel B")
-	go fanInConsumer(chanC, "Channel C")
+	go producer(chanA, "Channel A")
+	go producer(chanB, "Channel B")
+	go consumer(chanC, "Channel C")
 
 	for {
 		select {
@@ -52,25 +53,6 @@ func FanInExample() {
 }
 
 // FAN OUT
-func fanOutProducer(ch chan <- int, name string)  {
-	for {
-		// generate random number
-		n := rand.Intn(100)
-		// sleep for some time to simulate work
-		time.Sleep(time.Duration(n) * 2 * time.Millisecond)
-		// write the generated random number to the channel
-		fmt.Printf("%s -> %d\n", name, n)
-		ch <- n
-	}
-
-}
-
-func fanOutConsumer(ch <- chan int, name string) {
-	for n := range ch {
-		fmt.Printf("%s <- %d\n", name, n)
-	}
-}
-
 func FanOutExample() {
 	var n int
 
@@ -78,17 +60,10 @@ func FanOutExample() {
 	chanB := make(chan  int)
 	chanC := make(chan  int)
 
-	go fanOutProducer(chanA, "Channel A")
-	go fanOutConsumer(chanB, "Channel B")
-	go fanOutConsumer(chanC, "Channel C")
+	go producer(chanA, "Channel A")
+	go consumer(chanB, "Channel B")
+	go consumer(chanC, "Channel C")
 
-	//for {
-	//	select {
-	//	case n = <- chanA:
-	//		chanC <- n
-	//	case n = <- chanB:
-	//		chanC <- n
-	//	}
 	for n = range chanA {
 		if n < 50 {
 			chanB <- n
